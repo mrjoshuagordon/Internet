17e212a01c6ddfc352ed4168f9d16d560dc603b9
mrjoshuagordon <mr.joshuagordon@gmail.com>
2014-04-22 20:20:09 -0700

    orlando

---
+++ orlando.R
@@ -0,0 +1,45 @@
+setwd("/Users/Joshua/Desktop/Internet")
+
+library(maps)
+map('state', region="florida")
+
+orlando = c(28.37,-81.5568)
+miami = c(25.6132,-80.3474)
+
+points(orlando[2], orlando[1], col="red", cex=1, pch=16)
+text(orlando[2]+.2, orlando[1]+.2, col="black", "Orlando")
+
+points(miami[2], miami[1], col="blue", cex=1, pch=16)
+text(miami[2]-.5, miami[1]+.2, col="black", "Miami")
+
+x = orlando[1]
+y = orlando[2]
+m = (orlando[2] - miami[2])/((orlando[1] - miami[1]))
+b =  y - m*x 
+
+y.new = seq(miami[1],orlando[1], by=.01)
+x.new = y.new*m + b
+
+lines(x.new, y.new, lty=2, col="brown")
+
+zipcode = na.omit(read.csv("zipcode.csv"))
+
+x.top = x.new+.2
+x.bottom = x.new-.2
+lines(x.top, y.new, lty=2, col="red") 
+lines(x.bottom, y.new, lty=2, col="red") 
+
+
+answer = c()
+for(i in 1:length(x.bottom)){
+answer= c(answer, which(zipcode$latitude < max(y.new) & zipcode$latitude > min(y.new) & zipcode$longitude < x.bottom[i] & zipcode$longitude > x.top[i]))
+}
+
+new.zipcode = zipcode[answer,]
+
+nzc = new.zipcode
+
+points(nzc$longitude, nzc$latitude, col="green")
+
+
+
